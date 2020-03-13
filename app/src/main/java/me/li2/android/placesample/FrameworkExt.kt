20b84974e9961c467abc8616e2bf0c289be35678
weiyi.li <weiyi.just2@gmail.com>
2020-03-13 22:14:29 +1300

    Added maps module

--- app/src/main/java/me/li2/android/placesample/FrameworkExt.kt
+++ app/src/main/java/me/li2/android/placesample/FrameworkExt.kt
@@ -2,9 +2,11 @@ package me.li2.android.placesample
 
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.jakewharton.rxbinding3.widget.textChanges
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -12,8 +14,8 @@ import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 fun <T> Observable<T>.forUi(): Observable<T> =
-        this.observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
+    this.observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
 
 fun <T> Observable<T>.throttleFirstShort() = this.throttleFirst(500L, TimeUnit.MILLISECONDS)!!
 
@@ -26,15 +28,23 @@ fun <T> Observable<T>.throttleFirstShort() = this.throttleFirst(500L, TimeUnit.M
  */
 fun EditText.queryTextChanges(): Observable<String> {
     return textChanges()
-            .map { it.toString() }
-            .filter { it.isNotBlank() }
-            .debounce(300, TimeUnit.MILLISECONDS)
-            .distinctUntilChanged()
+        .map { it.toString() }
+        .filter { it.isNotBlank() }
+        .debounce(300, TimeUnit.MILLISECONDS)
+        .distinctUntilChanged()
 }
 
 fun Fragment.toast(message: String) = Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 
 @BindingAdapter("android:visibility")
 fun setViewVisibility(view: View, value: Boolean?) {
-        view.visibility = if (value == true) View.VISIBLE else View.GONE
+    view.visibility = if (value == true) View.VISIBLE else View.GONE
+}
+
+
+@BindingAdapter("android:src")
+fun setImageUrl(view: ImageView, src: String) {
+    Glide.with(view.context)
+        .load(src)
+        .into(view)
 }
